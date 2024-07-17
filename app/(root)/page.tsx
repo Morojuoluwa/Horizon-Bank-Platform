5021cbbc93c26e77fbbc072cc7f4553645ee870e
abimbola patrick <abimbolapatrickmj@gmail.com>
2024-07-17 15:57:07 -0700

    Built the right sidebar

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,11 @@
 import HeaderBox from '@/components/HeaderBox'
+import RightSidebar from '@/components/RightSidebar'
 import TotalBalanceBox from '@/components/TotalBalanceBox'
 import React from 'react'
 
 const Home = () => {
 
-    const loggedIn = {firstname:"Moroju"}
+    const loggedIn = {firstName:"Moroju", lastName:"abimbola", email:"patoo@gmail.com"}
   return (
     <section className='home'>
         <div className=' home-content'>
@@ -12,7 +13,7 @@ const Home = () => {
                 <HeaderBox
                 type="greeting"
                 title="Welcome"
-                user={loggedIn?.firstname || 'Guest'}
+                user={loggedIn?.firstName || 'Guest'}
                 subtext= "Access and manage your account and transactions efficiently"
                 
                 />
@@ -26,6 +27,11 @@ const Home = () => {
             </header>
 
         </div>
+        <RightSidebar
+        user={loggedIn}
+        transactions={[]}
+        banks={[{}, {}]}
+        />
 
     </section>
   )
